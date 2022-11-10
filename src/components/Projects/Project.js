3504ab5969667ff3a70f00f9b0ee9b774b1d7c7f
Jamie Vos <Jamievos100@gmail.com>
2022-11-10 22:59:14 +0100

    class component

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,19 +1,18 @@
 import React from 'react'
 
-function Project(props) {
+const Project = (props) => {
 
-  return (
-    <a class="card" onclick="/#">
-        <div class="card__background" style={{ backgroundImage: `url(${props.img})` }}>
-            <div class="card-info">
-                <i class={props.fa1} ></i>
-                <i class={props.fa2} ></i>
-                <i class={props.fa3} ></i>
+    return (
+        <div class="card">
+            <div class="card__background" style={{ backgroundImage: `url(${props.img})` }}>
+                <div class="card-info">
+                    <i class={props.fa1} ></i>
+                    <i class={props.fa2} ></i>
+                    <i class={props.fa3} ></i>
+                </div>
             </div>
         </div>
-        <div class="card__content"></div>
-    </a>
-  )
-}
+    )
+ }
 
-export default Project
+export default Project;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,8 @@
 import "./Projects.css";
 import Project from "./Project";
+import React from "react";
 
-const Projects = (props) => {
+const Projects = () => {
 
     return (
         <section className="projects__section">
@@ -10,12 +11,13 @@ const Projects = (props) => {
                 <Project fa1="fa-brands fa-css3" fa2="fa-brands fa-js" fa3="fa-brands fa-react" img="https://img.freepik.com/vrije-photo/groothoekopname-van-een-enkele-boom-die-groeit-onder-een-bewolkte-hemel-tijdens-een-zonsondergang-omgeven-door-gras_181624-22807.jpg?w=2000"/>
                 <Project fa1="fa-brands fa-css3" fa2="fa-brands fa-js" fa3="fa-brands fa-react" img="https://img.freepik.com/vrije-photo/groothoekopname-van-een-enkele-boom-die-groeit-onder-een-bewolkte-hemel-tijdens-een-zonsondergang-omgeven-door-gras_181624-22807.jpg?w=2000"/>
                 <Project fa1="fa-brands fa-css3" fa2="fa-brands fa-js" fa3="fa-brands fa-react" img="https://img.freepik.com/vrije-photo/groothoekopname-van-een-enkele-boom-die-groeit-onder-een-bewolkte-hemel-tijdens-een-zonsondergang-omgeven-door-gras_181624-22807.jpg?w=2000"/>
-                <Project fa1="fa-brands fa-js" fa2="fa-brands fa-html5" fa3="fa-brands fa-sass" img="https://img.freepik.com/vrije-photo/groothoekopname-van-een-enkele-boom-die-groeit-onder-een-bewolkte-hemel-tijdens-een-zonsondergang-omgeven-door-gras_181624-22807.jpg?w=2000"/>
-                <Project fa1="fa-brands fa-js" fa2="fa-brands fa-html5" fa3="fa-brands fa-sass" img="https://img.freepik.com/vrije-photo/groothoekopname-van-een-enkele-boom-die-groeit-onder-een-bewolkte-hemel-tijdens-een-zonsondergang-omgeven-door-gras_181624-22807.jpg?w=2000"/>
+                <Project fa1="fa-brands fa-unity" fa2="fa-brands fa-c" fa3="fa-brands fa-trello" img="https://img.freepik.com/vrije-photo/groothoekopname-van-een-enkele-boom-die-groeit-onder-een-bewolkte-hemel-tijdens-een-zonsondergang-omgeven-door-gras_181624-22807.jpg?w=2000"/>
+                <Project fa1="fa-brands fa-git" fa2="fa-brands fa-html5" fa3="fa-brands fa-sass" img="https://img.freepik.com/vrije-photo/groothoekopname-van-een-enkele-boom-die-groeit-onder-een-bewolkte-hemel-tijdens-een-zonsondergang-omgeven-door-gras_181624-22807.jpg?w=2000"/>
                 <Project fa1="fa-brands fa-js" fa2="fa-brands fa-html5" fa3="fa-brands fa-sass" img="https://img.freepik.com/vrije-photo/groothoekopname-van-een-enkele-boom-die-groeit-onder-een-bewolkte-hemel-tijdens-een-zonsondergang-omgeven-door-gras_181624-22807.jpg?w=2000"/>
                 <Project fa2="fa-brands fa-python" img="https://img.freepik.com/vrije-photo/groothoekopname-van-een-enkele-boom-die-groeit-onder-een-bewolkte-hemel-tijdens-een-zonsondergang-omgeven-door-gras_181624-22807.jpg?w=2000"/>
             </div>
         </section>
+
     );
 }
 
