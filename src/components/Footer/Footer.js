8eba091c0f81bb41c6f033799ff78310fbbbc2ba
Jamie Vos <Jamievos100@gmail.com>
2022-11-23 16:25:16 +0100

    Nieuwe projecten + footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,18 @@ import "./Footer.css";
 const Footer = () => {
     return(
         <section className="section__footer">
-            <h1>Footer</h1>
+            <div className="footer__upper">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d77979.65162740376!2d4.833921041898723!3d52.35474979518866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c63fb5949a7755%3A0x6600fd4cb7c0af8d!2sAmsterdam!5e0!3m2!1snl!2snl!4v1669216343362!5m2!1snl!2snl" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" width={400} height={400}></iframe>
+            </div>        
+            <div className="footer__lower">
+                <p>
+                    © All Rights Reserved Jamie Vos
+                </p>
+                <div className="footer__lower--p">
+                    <p>Privacy policy</p>
+                    <p>Terms & condition</p>
+                </div>
+            </div>
         </section>
     );
 }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,20 +3,24 @@ import Project from "./Project";
 import React from "react";
 import PokemonApp from "../../img/PokemonApp.jpg";
 import PolitieZO from "../../img/PolitieZO.jpg";
+import Dashboard from "../../img/Dashboard.jpg";
+import SoundSensorApp from "../../img/SoundSensorApp.jpg";
+import Potion from "../../img/Potion.jpg";
+import Flowchart from "../../img/Flowchart.jpg";
 
 const Projects = () => {
 
     return (
         <section className="projects__section">
             <div class="card-grid">
-                <Project fa1="fa-brands fa-html5" fa2="fa-brands fa-css3" fa3="fa-brands fa-js" img="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/553.png" />
-                <Project fa1="fa-brands fa-css3" fa2="fa-brands fa-js" fa3="fa-brands fa-react" img={PolitieZO}/>
-                <Project fa1="fa-brands fa-css3" fa2="fa-brands fa-js" fa3="fa-brands fa-react" img="https://img.freepik.com/vrije-photo/groothoekopname-van-een-enkele-boom-die-groeit-onder-een-bewolkte-hemel-tijdens-een-zonsondergang-omgeven-door-gras_181624-22807.jpg?w=2000"/>
-                <Project fa1="fa-brands fa-css3" fa2="fa-brands fa-js" fa3="fa-brands fa-react" img="https://img.freepik.com/vrije-photo/groothoekopname-van-een-enkele-boom-die-groeit-onder-een-bewolkte-hemel-tijdens-een-zonsondergang-omgeven-door-gras_181624-22807.jpg?w=2000"/>
-                <Project fa1="fa-brands fa-unity" fa2="fa-brands fa-c" fa3="fa-brands fa-trello" img="https://img.freepik.com/vrije-photo/groothoekopname-van-een-enkele-boom-die-groeit-onder-een-bewolkte-hemel-tijdens-een-zonsondergang-omgeven-door-gras_181624-22807.jpg?w=2000"/>
-                <Project fa1="fa-brands fa-git" fa2="fa-brands fa-html5" fa3="fa-brands fa-sass" img="https://img.freepik.com/vrije-photo/groothoekopname-van-een-enkele-boom-die-groeit-onder-een-bewolkte-hemel-tijdens-een-zonsondergang-omgeven-door-gras_181624-22807.jpg?w=2000"/>
-                <Project fa1="fa-brands fa-js" fa2="fa-brands fa-html5" fa3="fa-brands fa-sass" img="https://img.freepik.com/vrije-photo/groothoekopname-van-een-enkele-boom-die-groeit-onder-een-bewolkte-hemel-tijdens-een-zonsondergang-omgeven-door-gras_181624-22807.jpg?w=2000"/>
-                <Project fa2="fa-brands fa-python" img="https://img.freepik.com/vrije-photo/groothoekopname-van-een-enkele-boom-die-groeit-onder-een-bewolkte-hemel-tijdens-een-zonsondergang-omgeven-door-gras_181624-22807.jpg?w=2000"/>
+                <Project fa1="fa-brands fa-css3" fa2="fa-brands fa-html5" fa3="fa-brands fa-react" img={PokemonApp} />
+                <Project fa1="fa-brands fa-css3" fa2="fa-brands fa-html5" fa3="fa-brands fa-js" img={PolitieZO}/>
+                <Project fa1="fa-brands fa-css3" fa2="fa-brands fa-html5" fa3="fa-brands fa-react" img={Dashboard}/>
+                <Project fa2="fa-brands fa-c" img={SoundSensorApp}/>
+                <Project fa1="fa-brands fa-unity" fa2="fa-brands fa-c" fa3="fa-brands fa-trello" img={Potion}/>
+                <Project fa1="fa-brands fa-react" fa2="fa-brands fa-html5" fa3="fa-brands fa-sass" img="https://i.pinimg.com/564x/ec/2f/9e/ec2f9e23a043375c8c5b735f992cf22a--michael-jordan-michael-okeefe.jpg"/>
+                <Project fa2="fa-brands fa-python" img={Flowchart}/>
+                <Project fa1="fa-brands fa-php" fa2="fa-brands fa-css3" fa3="fa-brands fa-html5" img="https://d1fdloi71mui9q.cloudfront.net/BjJDjS8yQ6WnGSODhM1A_46t7s8wpvrliKYH8"/>
             </div>
         </section>
 
