907d4e5d93d9a78451b6d2d38a6261c0b5feebeb
Jamie Vos <Jamievos100@gmail.com>
2022-11-28 16:31:43 +0100

    merge

--- src/components/Blog/Blogs.js
+++ src/components/Blog/Blogs.js
@@ -6,7 +6,7 @@ function Blogs() {
         <div class="post-box mobile">
             <img src="../Foto's/cs.jpg" alt="" class="post-img" />
             <h2 class="category">Mobile</h2>
-            <a href="#" class="post-title">
+            <a href="/#" class="post-title">
                 Business development
             </a>
             <span class="post-date">8 JUL 2022</span>
